43b663fcd5ebb1629e026725e3c2cb57be0ed5a1
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-03-27 21:49:59 +0530

    Create S.java

---
+++ S.java
@@ -0,0 +1,17 @@
+public class Prog {
+    public static void main(String[] args) {
+        int n = 5; // Size of the letter and the square
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // Conditions for printing '*' to form the letter S in a square pattern
+                if (i == 0 || i == n / 2 || i == n - 1 || (i < n / 2 && j == 0) || (i > n / 2 && j == n - 1)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
