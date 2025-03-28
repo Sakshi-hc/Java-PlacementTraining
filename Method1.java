5e72eb6514b673716498a33eb00023f71f452fce
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-03-28 09:48:12 +0530

    Add files via upload

---
+++ Method1.java
@@ -0,0 +1,16 @@
+
+package com.smvitm;
+public class Method1{
+    static void greetings()
+    {
+        System.out.println("Happy Morning");
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Program Starts");
+        greetings();
+        System.out.println("Program Ends");
+    }
+    
+}
