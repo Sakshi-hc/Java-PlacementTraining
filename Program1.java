752aa0955959f5c647b032d64c70e4df32a049e5
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-03-28 10:42:31 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,17 @@
+
+package com.scanner.smvitm;
+import java.util.Scanner;
+public class Program1 {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the Value");
+        int A=obj.nextInt();
+        System.out.println("The value of A:"+A);
+        obj.close();
+    }
+    
+}
+
+
+    
+    
---
+++ Program2.java
@@ -0,0 +1,12 @@
+package com.scanner.smvitm;
+import java.util.Scanner;
+public class Program2 {
+    public static void main(String[] args)
+    {
+        Scanner scn=new Scanner(System.in);
+        int a=scn.nextInt();
+        int b=scn.nextInt();
+        double c=scn.nextDouble();
+        System.out.println(a+" "+b+" "+c);
+    }
+}
