190591f07b60de1937ae113410d95fec96f93e20
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-04-04 10:03:39 +0530

    Add files via upload

---
+++ Program10.java
@@ -0,0 +1,20 @@
+
+package program10;
+
+
+public class Program10 {
+    static void mul(int i,int j,int k)
+    {
+        System.out.println(i*j*k);
+    }
+
+    
+    public static void main(String[] args) {
+        System.out.println("Program starts");
+        mul(2,2,3);
+        mul(2,4,2);
+        System.out.println("Program ends");
+       
+    }
+    
+}
