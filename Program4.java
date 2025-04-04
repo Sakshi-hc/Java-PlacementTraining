e4b0f4724c962f103672a5a772949c33b6863cd7
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-04-04 10:00:57 +0530

    Add files via upload

---
+++ Program4.java
@@ -0,0 +1,18 @@
+
+package program4;
+
+
+public class Program4 {
+
+   
+    public static void main(String[] args) {
+     int n=5;
+     char ch='a';
+     for(int i=0;i<n;i++)
+     {
+         System.out.print(ch+" ");
+         ch++;
+     }
+    }
+    
+}
