f38562ec683d966a23cddbba815ee720702dedaf
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-04-04 10:00:18 +0530

    Add files via upload

---
+++ Program3.java
@@ -0,0 +1,15 @@
+
+package program3;
+
+
+public class Program3 {
+
+   
+    public static void main(String[] args) {
+        for(int n=10;n>0;n=n-3)
+        {
+            System.out.println("Chennai");
+        }
+    }
+    
+}
