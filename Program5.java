8b5177b32d7b0829f3661ca6683d1d84dc16048c
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-04-04 10:01:18 +0530

    Add files via upload

---
+++ Program5.java
@@ -0,0 +1,25 @@
+
+package program5;
+
+
+public class Program5 {
+
+    
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=3;i++)
+        {
+            System.out.println(i+" ");
+            if(i<5/2)
+            {
+                i++;
+            }
+                else{
+                
+                        i--;
+                        }
+            }
+        }
+    }
+    
+
