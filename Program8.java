ec5e1b23f2dc2184876e0251137dbe1b57d34dd3
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-04-04 10:02:35 +0530

    Add files via upload

---
+++ Program8.java
@@ -0,0 +1,21 @@
+
+package program8;
+
+
+public class Program8 {
+    static void add(int i,int j)
+    {
+        System.out.println(i+j);
+    }
+          
+    
+public static void main(String[] args) {
+    System.out.println("Program starts");
+    add(12,3);
+    add(6,3);
+    add(4,4);
+    System.out.println("program ends");
+        
+    }
+    
+}
