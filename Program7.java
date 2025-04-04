f2481496a3b096c8200dc1f1af878d2292c29cba
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-04-04 10:02:15 +0530

    Add files via upload

---
+++ Program7.java
@@ -0,0 +1,19 @@
+
+package program7;
+
+
+public class Program7 {
+    static void greetings()
+    {
+        System.out.println("hello Learners");
+    }
+
+   
+    public static void main(String[] args) {
+        System.out.println("Program starts");
+        greetings();
+        System.out.println("Program ends");
+        
+    }
+    
+}
