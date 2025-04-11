047002960e0941702d16fca7c4c22e9ebdf7024b
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-04-11 10:16:32 +0530

    Add files via upload

---
+++ Automorphic.java
@@ -0,0 +1,18 @@
+
+package numberproblems;
+
+public class Automorphic {
+    public static void main(String[] args)
+    {
+        int a=5;
+        int sq=a*a;
+        if(sq%10==a)
+        {
+            System.out.println("Automorphic");
+            
+        }
+        else{
+            System.out.println("Not Automorphic");
+        }
+    }
+}
