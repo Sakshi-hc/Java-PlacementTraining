5b0057bc3b3b68b8019e4fca29c5b4d9b3b53572
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-04-22 12:06:21 +0530

    Add files via upload

---
+++ Container.java
@@ -0,0 +1,21 @@
+
+package constructors;
+//Parameterized Constructor
+public class Container {
+    int capacity;
+    Container(int a)
+    {
+        capacity = a;
+        
+    }
+    public static void main(String[] args)
+    {
+        Container c = new Container(500);
+        System.out.println(c.capacity);
+        Container c1 = new Container(1500);
+        System.out.println(c1.capacity);
+        Container c2 = new Container(2000);
+        System.out.println(c2.capacity);
+    }
+            
+}
