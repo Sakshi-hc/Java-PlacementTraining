90109ee7dae2f0378815a272299242255ef776ca
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-04-22 12:06:10 +0530

    Add files via upload

---
+++ Alpha.java
@@ -0,0 +1,16 @@
+
+package constructors;
+//Default Constructor
+public class Alpha
+{
+    int n;
+    double d;
+    boolean b;
+    public static void main(String[] args)
+    {
+        Alpha a= new Alpha();
+        System.out.println(a.n+" "+a.d+" "+a.b  );
+    }
+    
+    
+}
