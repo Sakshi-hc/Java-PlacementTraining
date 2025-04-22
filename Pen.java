28cf38200fb326ade124ed6e518e0f1a55d09d68
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-04-22 11:36:00 +0530

    Add files via upload

---
+++ Pen.java
@@ -0,0 +1,16 @@
+
+package constructors;
+//zero argument constructor
+public class Pen {
+    int price;
+    Pen()
+{
+    price=10;
+    System.out.println(price);
+}
+
+    public static void main(String[] args) {
+       Pen p=new Pen();
+    }
+    
+}
