cf8347cbd0402cdda5b714412d2b61316f3c04f5
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-04-29 13:01:38 +0530

    Add files via upload

--- InstagramMain.java
+++ InstagramMain.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package polymorphism;
 
-/**
- *
- * @author SMV
- */
-public class Instagram {
-    
+package Polymorphism;
+
+//OVERRIDING
+class instagram1{
+    void post(){
+        System.out.println("Photos can be posted");
+    }
+}
+class instagram2 extends instagram1{
+    @Override
+    void post(){
+        super.post();
+        System.out.println("Photos and videos can be posted");
+    }
+}
+class instagram3 extends instagram2{
+    @Override
+    void post(){
+        super.post();
+        System.out.println("Photos,videos  and Reels can be posted");
+    }
+}
+
+public class InstagramMain {
+public static void main(String[] args) {
+instagram3 in = new instagram3();
+ in.post();
+}
 }
