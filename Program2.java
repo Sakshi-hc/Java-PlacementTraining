1fece6bcd093c2e17b47ba4ab4bf8f65eb050e64
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-05-02 10:06:51 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,20 @@
+
+package String;
+
+public class Program2 {
+    public static void main(String[] args){
+        String s1="java";
+        String s2="Java";
+        
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase(s2));
+        char[] arr=s1.toCharArray();
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+        System.out.println(s1.indexOf('a'));
+        System.out.println(s1.lastIndexOf('a'));
+    }
+}
